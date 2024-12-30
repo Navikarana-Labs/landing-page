fe532c90199f5edd60bdc78bfc0c7ab1e88b28b8
Code Alchamy <kashyapb.1905@gmail.com>
2024-12-30 21:29:36 +0530

    added contact infos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FaGithub, FaXTwitter } from "react-icons/fa6";
+import { MdAlternateEmail } from "react-icons/md";
 
 const Home = () => {
   return (
@@ -17,8 +19,25 @@ const Home = () => {
         height={512}
         className="hidden dark:block w-96 h-auto"
       />
-      <h1 className="text-5xl mt-4 font-bold">Navīkaraṇa Labs</h1>
-      <p className="text-lg mt-2">Landing page for official website of Navīkaraṇa Labs.</p>
+      <section className="mt-4 mb-10">
+        <h1 className="text-5xl font-bold">Navīkaraṇa Labs</h1>
+        <h2 className="text-xl mt-2">Something extraordinary is on the horizon.</h2>
+      </section>
+
+      <footer className="flex flex-col justify-center items-center gap-2">
+        <p className="text-lg border-b mb-2 leading-none">Get in touch with us</p>
+        <div className="flex items-center gap-4 justify-center">
+          <a href="https://x.com/navikarana_labs" target="_blank" rel="noopener noreferrer" className="text-xl">
+            <FaXTwitter />
+          </a>
+          <a href="https://github.com/Navikarana-Labs" target="_blank" rel="noopener noreferrer" className="text-xl">
+            <FaGithub />
+          </a>
+          <a href="mailto:hello@navikarana.io" className="text-2xl">
+            <MdAlternateEmail />
+          </a>
+        </div>
+      </footer>
     </main>
   );
 };
