728cd1f9ebadaf54f6d23484f0b69a4d17f1932d
Lokesh Kumar <hi@loke.sh>
2025-04-12 15:47:54 +0530

    removed cta for now - will be back soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { WaitListForm } from "@/components/forms";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
@@ -6,19 +7,32 @@ const Home = () => {
   return (
     <React.Fragment>
       <section className="flex flex-col items-center justify-center">
-        <h1 className="text-3xl md:text-5xl font-bold">Navīkaraṇa Labs</h1>
+        <Link href="/" className="flex items-center space-x-1">
+          <Image
+            src="/logomark-lightmode.svg"
+            alt="navikarana labs logo"
+            width={52}
+            height={52}
+            className="hidden md:block"
+          />
+          <h1 className="text-3xl md:text-5xl font-bold">Navīkaraṇa Labs</h1>
+        </Link>
         <h3 className="text-base text-center md:text-lg font-medium mt-4 text-foreground/80">
-          This ship is about to take off — be part of the adventure!
+          Something extraordinary is on the horizon. Join us on this journey.
         </h3>
       </section>
+      {/*
       <section className="flex flex-col items-center justify-center mt-12">
         <h2 className="text-lg md:text-xl leading-tight text-center mb-4 font-medium text-foreground">
           Join the waitlist to get early access.
         </h2>
         <WaitListForm />
       </section>
+            */}
       <section className="mt-6 flex flex-col justify-center items-center gap-2">
-        <h2 className="text-base md:text-lg mb-2 font-medium text-foreground border-b border-current">Follow us on</h2>
+        <h2 className="text-base md:text-lg mb-2 font-medium text-foreground border-b border-current">
+          Follow us on
+        </h2>
         <div className="flex items-center gap-5 justify-center">
           <a
             href="https://x.com/navikarana_labs"
